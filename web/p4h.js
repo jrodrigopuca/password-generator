225a55d38755a99d7917c50b9b6329eab180e2ba
J.A. Rodrigo Puca <r@geeks.ms>
2019-05-27 20:11:57 -0300

    5/27

---
+++ web/p4h.js
@@ -0,0 +1,69 @@
+function getWord(array) {
+    let min = 0; let max = array.length - 1;
+    let random = Math.floor(Math.random() * (max - min)) + min;
+    return array[random]
+}
+
+function change(text, aOriginal, aChanged) {
+    let index = -1;
+
+    while (index < 0) {
+        char = getWord(text)
+        console.log(char)
+        index = aOriginal.indexOf(char)
+    }
+
+    text = text.replace(char, aChanged[index]);
+    return text;
+}
+
+function getNumber(text) {
+    let original = ["a", "b", "e", "i", "g", "o", "q", "s", "t", "z"];
+    let numbers = [4, 8, 3, 1, 6, 0, 9, 5, 7, 2];
+    return change(text, original, numbers);
+}
+
+function getSymbol(text) {
+    let original = ["a", "c", "d", "i", "l", "o", "p", "s", "y"];
+    let symbols = ["@", "(", ")", "!", "/", "*", "?", "$", "&"];
+    return change(text, original, symbols);
+}
+
+function getMayus(text) {
+    let az = "abcdefghijklmnopqrstuvwxyz";
+    let original = Array.from(az)
+    let AZ = Array.from(az.toUpperCase())
+    return change(text, original, AZ)
+}
+
+function valid(text) {
+    const expression = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.@*\s).{8,15}$/i;
+    return expression.test(text);
+}
+
+function getWords() {
+    fetch('https://raw.githubusercontent.com/words/an-array-of-spanish-words/master/palabras.json')
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+
+            let aWord=getWord(data);
+            
+            while ( aWord.length< 8 || aWord.length>15) {
+                aWord=getWord(data);
+                console.log(aWord);
+            }
+            console.log(aWord);
+            aWord=getSymbol(aWord)
+            aWord=getNumber(aWord)
+            aWord=getMayus(aWord)
+            console.log(aWord);            
+
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+getWords();
