1c56321face9ba7ded82addf1d2e123ae7836e97
J.A. Rodrigo Puca <r@geeks.ms>
2020-02-05 23:39:39 -0300

    v0.1 componente

---
+++ extension/popup.js
@@ -0,0 +1,19 @@
+i=0;
+
+let btn = document.getElementById('btnRefresh');
+let lbl = document.getElementById('lblResult');
+
+btn.addEventListener('click', e => changeValue());
+
+async function changeValue() {
+    i=i+1;
+    lbl.innerHTML = i+ "Valor:" + Math.random();
+
+}
+
+async function init() {
+    changeValue();
+}
+
+init();
+
---
+++ extension/words.js
@@ -0,0 +1,3 @@
+export default {
+    ["abaters","abates","abating","abatis","abatises","abator","abators","abattis","abattises","abattoir","abattoirs","abattu","abature","abatures","abaxial"]
+}
